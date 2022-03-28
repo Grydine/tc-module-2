d564878912e72ab9da1fe7d0d4c8d2aec2cc097e
Arif Ilham Maulana <76241878+Grydine@users.noreply.github.com>
2022-03-28 15:19:30 +0700

    commit tgs 3 module 2

--- src/components/track/track.component.jsx
+++ src/components/track/track.component.jsx
@@ -1,6 +1,5 @@
 import "./track.styles.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCheck } from "@fortawesome/free-solid-svg-icons";
+
 
 export default function TrackComponent({ image, title, artist }) {
   return (
@@ -12,7 +11,7 @@ export default function TrackComponent({ image, title, artist }) {
         <p>{title}</p>
         <p>{artist}</p>
         <button className="button-song">
-          <FontAwesomeIcon icon={faCheck} className="fa" />
+        
           Select
         </button>
       </div>
